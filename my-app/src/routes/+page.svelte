dd98598e9eecb5b882cce3244beceb33a66d15e7
zhourui guo <424013536@qq.com>
2024-09-03 15:16:18 +0200

    flip the scale

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,12 +1,12 @@
 <script>
 	export let data = [];
+  
+	const rescale = function(x, domain_min, domain_max, range_min, range_max) {
+		return ((range_max - range_min)*(x-domain_min))/(domain_max-domain_min) + range_min
+	}
   </script>
   
   <style>
-	svg {
-		border: 1px;
-		border-style: solid;
-	}
 	circle {
 		fill: steelblue;
 		fill-opacity: 0.5;
@@ -15,6 +15,8 @@
   
   <svg width="800" height="400">
 	{#each data.flights as datapoint}
-		<circle cx={datapoint.from_long} cy={datapoint.from_lat} r="2" />
+		<circle cx={rescale(datapoint.from_long, -180, 180, 0, 800)}
+				cy={rescale(datapoint.from_lat, -90, 90, 400, 0)}
+				r=3 />
 	{/each}
   </svg>
