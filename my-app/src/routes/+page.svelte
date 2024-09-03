bfd42965fded077d2e5d99dea49fa67f00a488db
zhourui guo <424013536@qq.com>
2024-09-03 15:59:10 +0200

    color & size

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,22 +1,32 @@
 <script>
+	import { scaleLinear } from 'd3-scale';
+  
 	export let data = [];
   
-	const rescale = function(x, domain_min, domain_max, range_min, range_max) {
-		return ((range_max - range_min)*(x-domain_min))/(domain_max-domain_min) + range_min
-	}
-  </script>
+	const scaleX = scaleLinear().domain([-180,180]).range([0,800])
+	const scaleY = scaleLinear().domain([-90,90]).range([400,0])
+	const scaleRadius = scaleLinear().domain([1,15406]).range([2,10])
+	</script>
   
   <style>
+	svg {
+	  border: 1px;
+	  border-style: solid;
+	}
 	circle {
-		fill: steelblue;
-		fill-opacity: 0.5;
+	  fill: steelblue;
+	  fill-opacity: 0.5;
+	}
+	circle.international { 
+	  fill: red;
 	}
   </style>
   
   <svg width="800" height="400">
 	{#each data.flights as datapoint}
-		<circle cx={rescale(datapoint.from_long, -180, 180, 0, 800)}
-				cy={rescale(datapoint.from_lat, -90, 90, 400, 0)}
-				r=3 />
+	  <circle cx={scaleX(datapoint.from_long)}
+		  cy={scaleY(datapoint.from_lat)}
+		  r={scaleRadius(datapoint.distance)}
+		  class:international={datapoint.from_country != datapoint.to_country}/> 
 	{/each}
   </svg>
